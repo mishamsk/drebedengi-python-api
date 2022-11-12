5ba20d67752abe3dd43ffbe542a9235047915020
mishamsk <mishamsk@gmail.com>
2022-11-12 16:05:28 -0500

    Bump version: 0.1.0 → 0.2.0

--- src/drebedengi/__init__.py
+++ src/drebedengi/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for Drebedengi Python API."""
 __author__ = """Mike Perlov"""
 __email__ = "mishamsk@gmail.com"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from .api import DrebedengiAPI
 
